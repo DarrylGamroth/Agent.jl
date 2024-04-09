aabb852432189fd01e93b61b8c8d67c1f66c6134
Darryl Gamroth <dgamroth@rubus.ca>
2024-04-08 17:22:16 -0700

    Rework atomic variables

--- src/agentrunner.jl
+++ src/agentrunner.jl
@@ -6,6 +6,7 @@ mutable struct AgentRunner{I<:IdleStrategy,A<:AbstractAgent}
     idle_strategy::I
     agent::A
     task::Task
+    @atomic is_started::Bool
     @atomic is_closed::Bool
     @atomic is_running::Bool
     AgentRunner{I,A}() where {I,A} = new()
@@ -26,8 +27,9 @@ function AgentRunner(idle_strategy::I, agent::A) where {I<:IdleStrategy,A<:Abstr
     runner = AgentRunner{I,A}()
     runner.idle_strategy = idle_strategy
     runner.agent = agent
+    @atomic runner.is_started = false
     @atomic runner.is_closed = false
-    @atomic runner.is_running = true
+    @atomic runner.is_running = false
     runner.task = @task run(runner)
     return runner
 end
@@ -35,13 +37,22 @@ end
 """
     start(runner::AgentRunner)
 
-Starts the agent runner by scheduling the agent task.
+    Start the Agent running. Start may be called only once and is invalid after close has been called.
 
 # Arguments
 - `runner::AgentRunner`: The agent runner object.
 
 """
 function start(runner::AgentRunner)
+    if is_closed(runner)
+        throw(ArgumentError("AgentRunner is closed"))
+    end
+
+    _, success = @atomicreplace runner.is_started false => true
+    if !success
+        throw(ArgumentError("AgentRunner is already started"))
+    end
+
     schedule(runner.task)
 end
 
@@ -56,11 +67,8 @@ This function will wait for the agent task to exit.
 - `runner::AgentRunner`: The agent runner object.
 """
 function close(runner::AgentRunner)
-    is_running!(runner, false)
-    while true
-        if is_closed(runner)
-            return
-        end
+    _, success = @atomicreplace :sequentially_consistent runner.is_closed false => true
+    if success
         wait(runner.task)
     end
 end
@@ -76,7 +84,7 @@ Check if the agent runner is closed.
 # Returns
 - `Bool`: `true` if the agent runner is closed, `false` otherwise.
 """
-is_closed(runner::AgentRunner) = @atomic runner.is_closed
+is_closed(runner::AgentRunner) = @atomic :acquire runner.is_closed
 
 """
     is_closed!(runner::AgentRunner, value::Bool)
@@ -90,7 +98,7 @@ Set the closed status of the agent runner.
 # Returns
 - `Nothing`
 """
-is_closed!(runner::AgentRunner, value::Bool) = @atomic runner.is_closed = value
+is_closed!(runner::AgentRunner, value::Bool) = @atomic :release runner.is_closed = value
 
 """
     is_running(runner::AgentRunner)
@@ -103,7 +111,7 @@ Check if the agent runner is running.
 # Returns
 - `Bool`: `true` if the agent runner is running, `false` otherwise.
 """
-is_running(runner::AgentRunner) = @atomic runner.is_running
+is_running(runner::AgentRunner) = @atomic :acquire runner.is_running
 
 """
     is_running!(runner::AgentRunner, value::Bool)
@@ -117,32 +125,28 @@ Set the running status of the agent runner.
 # Returns
 - `Nothing`
 """
-is_running!(runner::AgentRunner, value::Bool) = @atomic runner.is_running = value
+is_running!(runner::AgentRunner, value::Bool) = @atomic :release runner.is_running = value
 
 function run(runner::AgentRunner)
     try
+        is_running!(runner, true)
         try
             on_start(runner.agent)
         catch e
-            is_running!(runner, false)
             throw(e)
         end
 
-        while is_running(runner)
+        while !is_closed(runner)
             do_work(runner)
         end
 
         try
             on_close(runner.agent)
         catch e
-            println("Error in on_close")
             throw(e)
         end
     finally
-        if is_running(runner)
-            on_close(runner.agent)
-        end
-        is_closed!(runner, true)
+        is_running!(runner, false)
     end
 end
 
@@ -150,13 +154,13 @@ end
     agent = runner.agent
     idle_strategy = runner.idle_strategy
     try
-        while true
+        while !is_closed(runner)
             workcount = do_work(agent)
             idle(idle_strategy, workcount)
         end
     catch e
         if e isa AgentTerminationException
-            is_running!(runner, false)
+            is_closed!(runner, true)
         else
             on_error(agent, e)
         end
