646bd704a6e0cce17cd7d2069c9119f517c51849
Darryl Gamroth <dgamroth@rubus.ca>
2024-09-03 15:21:05 -0700

    Get cache line size from Hwloc

--- src/backoffidlestrategy.jl
+++ src/backoffidlestrategy.jl
@@ -1,3 +1,8 @@
+using Hwloc
+
+const CACHE_LINE_SIZE::Int = maximum(cachelinesize())
+const CACHE_LINE_PAD::Int = CACHE_LINE_SIZE - sizeof(Int64)
+
 @enum BackoffIdleState begin
     BACKOFF_IDLE_STATE_NOT_IDLE
     BACKOFF_IDLE_STATE_SPINNING
@@ -13,19 +18,19 @@ Idling strategy for threads when they have no work to do.
 Spin for `max_spins`, then `yield()` for `max_yields`, then `nanosleep(max_park_period_ns)` on an exponential backoff to `max_park_period_ns`.
 """
 mutable struct BackoffIdleStrategy <: IdleStrategy
-    const pad1::NTuple{64,Int8} # Padding to avoid false sharing
-    const max_spins::Int
-    const max_yields::Int
-    const min_park_period_ns::Int
-    const max_park_period_ns::Int
-    spins::Int
-    yields::Int
-    park_period_ns::Int
+    const pad1::NTuple{CACHE_LINE_PAD,Int8}
+    const max_spins::Int64
+    const max_yields::Int64
+    const min_park_period_ns::Int64
+    const max_park_period_ns::Int64
+    spins::Int64
+    yields::Int64
+    park_period_ns::Int64
     state::BackoffIdleState
-    const pad2::NTuple{64,Int8} # Padding to avoid false sharing
+    const pad2::NTuple{CACHE_LINE_PAD,Int8}
     function BackoffIdleStrategy(max_spins, max_yields, min_park_period_ns, max_park_period_ns)
-        new(ntuple(x -> Int8(0), 64), max_spins, max_yields, min_park_period_ns, max_park_period_ns,
-        0, 0, 0, BACKOFF_IDLE_STATE_NOT_IDLE, ntuple(x -> Int8(0), 64))
+        new(ntuple(x -> Int8(0), CACHE_LINE_PAD), max_spins, max_yields, min_park_period_ns, max_park_period_ns,
+            0, 0, 0, BACKOFF_IDLE_STATE_NOT_IDLE, ntuple(x -> Int8(0), CACHE_LINE_PAD))
     end
 end
 
